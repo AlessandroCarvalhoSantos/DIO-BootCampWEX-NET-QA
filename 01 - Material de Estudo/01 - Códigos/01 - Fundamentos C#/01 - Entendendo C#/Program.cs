787856f1d32f3279d0f6ddd466214d0431dca79d
AlessandroC.Santos <santoscarvalhoalessandro@gmail.com>
2023-10-11 23:32:20 -0300

    subindo atualizações de estudo dos fundamentos do c#

--- 01 - Material de Estudo/01 - Códigos/01 - Fundamentos C#/01 - Entendendo C#/Program.cs
+++ 01 - Material de Estudo/01 - Códigos/01 - Fundamentos C#/01 - Entendendo C#/Program.cs
@@ -1,9 +1,15 @@
-﻿public class Program
+﻿using Models;
+using ModelsPessoa;
+
+public class Program
 {
     public static void Main(string[] args)
     {
+        ModelsPessoa.Pessoa p1 = new ModelsPessoa.Pessoa();
+        Models.Pessoa p2 = new Models.Pessoa();
 
-
+        p2.Nome = "Alessandro";
+        p2.Apresentar();
 
     }
 }
