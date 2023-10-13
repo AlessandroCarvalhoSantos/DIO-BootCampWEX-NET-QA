819392432c71bd30a31cbfef968d3e6594130e8e
AlessandroC.Santos <santoscarvalhoalessandro@gmail.com>
2023-10-12 23:20:45 -0300

    Atualização de curso c#, códigos do módulo 3

---
+++ 01 - Material de Estudo/01 - Códigos/02 - Desafios de código/01 - Entradas e tipos/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Descrição
+Neste desafio, vamos criar um programa que simula um algoritmo para registrar falhas em testes de um sistema. O programa solicitará ao usuário que insira o nome do teste e a descrição do erro correspondente. Em seguida, ele exibirá uma mensagem do erro formatado.
+
+Entrada
+A entrada do programa consiste em duas strings. A primeira string deve conter o nome do teste que falhou, e a segunda string deve conter a descrição do erro ocorrido durante o teste.
+
+Saída
+A saída do programa consiste em uma string formatada da seguinte maneira:
+"O teste falhou: Descrição do erro: [descricaoDoErro]"
+*/
+
+using System;
+
+namespace SimulacaoFalhaTeste {
+  class Program {
+    static void Main(string[] args) {
+      // TODO: Solicite ao usuário que digite o nome do teste:
+
+      // TODO: Solicite ao usuário que digite a descrição do erro:
+          
+      var titulo = Console.ReadLine();
+      var descricaoDoErro = Console.ReadLine();
+      // Se o teste falhar, exibe a descrição do erro:
+      Console.WriteLine($"O teste falhou. Descricao: {descricaoDoErro}");
+    }
+  }
+}
---
+++ 01 - Material de Estudo/01 - Códigos/02 - Desafios de código/03 - Operadores/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AnaliseDadosTeste {
+  class Program {
+    static void Main(string[] args) {
+      // Solicita ao usuário a quantidade de testes bem-sucedidos:
+      int testesBemSucedidos = int.Parse(Console.ReadLine());
+
+      // Solicita ao usuário a quantidade de testes totais:
+      int testesTotais = int.Parse(Console.ReadLine());
+      
+      var taxa = (double)testesBemSucedidos/testesTotais * 100;
+      
+      if(taxa >= 80)
+        Console.WriteLine("A funcionalidade esta pronta para lancamento.");
+      else
+        Console.WriteLine("A funcionalidade nao esta pronta para lancamento.");
+
+      // TODO: Implemente as condições para o cálculo da taxa de sucesso:
+
+      // TODO: Implemente uma estrutura condicional (if/else) para avaliar a taxa de sucesso e tomar decisões com base nela:
+
+    }
+  }
+}
