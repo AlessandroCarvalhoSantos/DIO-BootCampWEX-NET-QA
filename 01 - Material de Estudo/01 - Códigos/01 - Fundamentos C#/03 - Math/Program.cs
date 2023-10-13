d080848a33a5b317b922429882aa3e1026577ad1
AlessandroC.Santos <santoscarvalhoalessandro@gmail.com>
2023-10-13 00:56:02 -0300

    Atualização de estudo c#, Operadores aritméticos

--- 01 - Material de Estudo/01 - Códigos/01 - Fundamentos C#/03 - Math/Program.cs
+++ 01 - Material de Estudo/01 - Códigos/01 - Fundamentos C#/03 - Math/Program.cs
@@ -4,7 +4,24 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        Calculadora calc = new Calculadora();
+
+        calc.Somar(2,2);
+        calc.Subtrair(2,2);
+        calc.Multiplicar(2,2);
+        calc.Dividir(2,2);
+        calc.RaizQuadrada(9);
+        calc.Seno(3);
+        calc.CoSeno(3);
+        calc.Tagente(3);
 
 
+        //Incremento e decremento
+        int contador = 5;
+        contador++; // Agora contador é 6
+
+        int contador2 = 5;
+        contador2--; // Agora contador é 4
+        
     }
 }
--- 01 - Material de Estudo/01 - Códigos/01 - Fundamentos C#/04 - Repetição/Models/Pessoa.cs
+++ 01 - Material de Estudo/01 - Códigos/01 - Fundamentos C#/04 - Repetição/Models/Pessoa.cs
@@ -1,4 +1,4 @@
-namespace ModelsPessoa;
+namespace Models;
 
 public class Pessoa
 {
